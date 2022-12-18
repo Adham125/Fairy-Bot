e2aa33b6f5fe3f0ec2e2107f001c6e73298752d7
Adham125 <ahn508@york.ac.uk>
2022-12-18 16:52:06 +0200

    roll dice
    error handling about if input is a string not working yet

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ client = commands.Bot(command_prefix='~', intents=intents)
 
 kevin_id = 336153574088376331
 my_id = 378212274084773889
-user_kevin = client.get_user(kevin_id)
+welcome_ch = 1053502894290255946
 
 
 @client.event
@@ -19,22 +19,42 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
-    pass
+    channel_welcome = client.get_channel(welcome_ch)
+    await channel_welcome.send("Welcome " + member.name + " To The Server!")
 
 
 @client.event
-async def on_member_leave(member):
-
+async def on_member_remove(member):
+    user_kevin = client.get_user(kevin_id)
     await user_kevin.send("Member: " + member.name + " Has Left The Server")
 
 
 @client.command()
 async def ping(ctx):
-    await my_id.send("hi")
+    user_kevin = client.get_user(kevin_id)
+    user_me = client.get_user(my_id)
+    await user_kevin.send("test")
+    await user_me.send("test")
 
 
 @client.command()
-async def rollDice(ctx):
-    await ctx.send(randint(1 , 100))
+async def roll(ctx, number=1):
+    if number < 1:
+        await ctx.send("輸入次數必須大於一!")
+    elif number > 10:
+        await ctx.send("每輪擲骰最多擲十次!")
+    elif number == 1:
+        await ctx.send("恭喜你骰到 " + str(randint(0, 100)))
+    else:
+        output = str(randint(0, 100))
+        for i in range(number-2):
+            output = output + ", " + str(randint(0, 100))
+        output = output + " 跟 " + str(randint(0, 100))
+        await ctx.send(output)
 
+
+@roll.error()
+async def roll_error(error, ctx):
+    if isinstance(error, ValueError):
+        await ctx.send("輸入次數必須為整數!")
 client.run("MTA1MzczMDcyNTUwNzEyMTE3Mg.G3MGCW.bXwlFlIOH-G2rHwKqeLfBMhX0QI3yUvCAXlOag")
